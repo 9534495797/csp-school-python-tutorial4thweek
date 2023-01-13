a0fdf7558fea351eae2a598ea7624fe581c25321
9534495797 <chaubeyankit927@gmail.com>
2023-01-13 20:45:11 +0530

    ok

--- randomnumber.py
+++ randomnumber.py
@@ -1,20 +1,29 @@
 import random
+while True:
+    user_input=input("your choice(rock,paper,scissors):")
+    random_num=["rock","paper","scissors"]
+    computer_input=random.choice(random_num)
+    print(f"you choose {user_input}, computr choose {computer_input}")
 
-user_input=input("Enter a choice:")
-action=["rock","paper","scissors"]
-machine_action=random.choice(action)
-if user_input==machine_action:
-    print("game is cancelled")
-elif user_input=="rock":
-    if machine_action=="scissors":
-        print("user is the winner")
-elif user_input=="paper":
-    if machine_action=="scissors":
-        print("machine is winner")
+    if user_input==computer_input:
+        print("match tie")
+    elif user_input=="rock":
+        if computer_input=="paper":
+            print("paper catch rock u win!")
+        else:
+            print("computer wins")
+    elif user_input=="paper":
+        if computer_input=="scissors":
+            print("computer wins")
+        else:
+            print("user wins")   
+    elif user_input=="scissors":
+        if computer_input=="rock":
+            print("computer wins") 
+        else:
+            print("user wins")
+    play_again=input(("y or n: " ))
+    if play_again=="y":
+        continue
     else:
-        print("user wins")
-elif user_input=="rock":
-    if machine_action=="paper":
-        print("user is winner")
-    else:
-        print("machine is winner")
+        break    
