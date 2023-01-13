85f0365c99b63aed4ea3bf57a5675b8be03bd2fa
9534495797 <chaubeyankit927@gmail.com>
2023-01-13 20:44:40 +0530

    ok

---
+++ fibonaccimain.py
@@ -0,0 +1,13 @@
+def fibonacciSeries(Number):
+    if(Number == 0):
+        return 0
+    elif(Number == 1):
+        return 1
+    else:
+        return (fibonacciSeries(Number - 1) + fibonacciSeries(Number - 2))
+
+
+n = int(input("your input:"))
+print("Fibonacci series:", end = ' ')
+for n in range(0, n):  
+   print(fibonacciSeries(n), end = ' ')
