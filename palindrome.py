b1af49b356945870f6716cfe1d7be3646a51817f
9534495797 <chaubeyankit927@gmail.com>
2023-01-27 18:39:16 +0530

    ok'

---
+++ palindrome.py
@@ -0,0 +1,31 @@
+# def isPalindrome(s):
+# 	return s == s[::-1]
+
+
+# # Driver code
+# s = "malayalam"
+# ans = isPalindrome(s)
+
+# if ans:
+# 	print("Yes")
+# else:
+# 	print("No")
+
+# def palindrome(n):
+#     return n==n[::-1]
+# s=input("word: ")
+# ans=palindrome(s)
+# if ans:
+#     print("yes")
+# else:
+#     print("no")
+
+
+def palindrome(n):
+    return n==n[::-1]
+s=input("word: ")
+ans=palindrome(s)
+if ans:
+    print("yes")
+else:
+    print("no")
