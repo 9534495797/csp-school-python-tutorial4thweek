a3a67f5f23722eb7da24cde3b19c9a4992dbfd68
9534495797 <chaubeyankit927@gmail.com>
2023-01-30 19:04:41 +0530

    ok

---
+++ looptable.py
@@ -0,0 +1,43 @@
+i=2
+while i<=20:
+    print(i)
+    i = i+2
+
+i=3
+while i<=30:
+    print(i)
+    i = i+3
+
+i=4
+while i<=40:
+    print(i)
+    i = i+4
+
+i=5
+while i<=50:
+    print(i)
+    i = i+5
+i=6
+while i<=60:
+    print(i)
+    i = i+6
+
+i=7
+while i<=70:
+    print(i)
+    i = i+7
+
+i=8
+while i<=80:
+    print(i)
+    i = i+8
+
+i=9
+while i<=90:
+    print(i)
+    i = i+9
+
+i=10
+while i<=100:
+    print(i)
+    i = i+10
